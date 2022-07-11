b58e89f2229b881fea6b02fc41dc34e0674518a6
OG <host.jok@mail.ru>
2022-07-11 17:49:56 +0300

    4.1. Разбор первой программы. [#159033 #319948]

--- src/main/java/ru/job4j/Studentinfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class Studentinfo {
-    public static void main(String[] args) {
-        System.out.println("Vladimir Popov");
-        System.out.println("15.11.1998");
-    }
-}
